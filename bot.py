618af02caa9e12c9c6765c9df615dbeabaccede5
9zh57nyyrs-pixel <9zh57nyyrs@privaterelay.appleid.com>
2026-04-24 23:33:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from claude_client import ClaudeClient
 from config import TELEGRAM_TOKEN
 
 logging.basicConfig(
-format=’%(asctime)s - %(name)s - %(levelname)s - %(message)s’,
+format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 level=logging.INFO
 )
 logger = logging.getLogger(**name**)
