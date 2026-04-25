878e99fc970d6012c1b36dc70aeb85324ec34d42
9zh57nyyrs-pixel <9zh57nyyrs@privaterelay.appleid.com>
2026-04-25 22:04:57 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN") or os.environ.get("TELEGRAM_TOKEN", "")
-GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "")
-DATABASE_PATH = "medical_bot.db"
