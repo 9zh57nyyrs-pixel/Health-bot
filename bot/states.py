e6a4c9dc028b83aa908db8aad7e5991a061b59c3
9zh57nyyrs-pixel <9zh57nyyrs@privaterelay.appleid.com>
2026-04-28 15:22:43 +0300

    Update states.py

--- bot/states.py
+++ bot/states.py
@@ -1,1 +1,14 @@
+from aiogram.fsm.state import State, StatesGroup
 
+class MedicalSurvey(StatesGroup):
+    greeting = State()
+    age = State()
+    gender = State()
+    main_complaint = State()
+    symptom_duration = State()
+    symptom_severity = State()
+    additional_symptoms = State()
+    chronic_diseases = State()
+    medications = State()
+    allergies = State()
+    analysis = State()
